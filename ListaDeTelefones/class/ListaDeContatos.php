7a38d9c7e11a9cc1ead0d550b08337a85053a1ea
Ana Luiza <45442311+analubarreto@users.noreply.github.com>
2019-03-08 22:21:18 -0300

    Mudando métodos

--- ListaDeTelefones/class/ListaDeContatos.php
+++ ListaDeTelefones/class/ListaDeContatos.php
@@ -1,47 +1,19 @@
 <?php
 
 class ListaDeContatos {
-    private $contato;
-    private $lista;
+    private $lista = array();
 
-    public function __construct($contato, $lista) {
-        $this->contato = $contato;
-        $this->lista = array();
+    public function __toString() {
+        return $this->lista;
     }
 
-    public function mostrarLista() {
-        foreach ($this->lista as $item) {
-            return $item . "<br/>";
-        }
+    public function adicionarItens($item) {
+        array_push($this->lista, $item);
     }
 
-    public function adicionarItens($vI) {
-        $max = sizeof($this->lista) - 1;
-    
-        if ($n < $max) {
-            $i = $this->lista[$i]; 
-            while ($i > 0 && $max > $vI) {
-                $this->lista[$i] = $this->lista[$i - 1];  
-                $i -= 1;
-            }
-            $this->lista[$i] = $vI;
-        } else {
-            return "Overflow";
-        }
-    }
-
-    public function removerItens($vI) {
-        if ($i < $this->lista[$i]) {
-            $this->lista[$i] -= 1; //$n
-    
-            while ($i < $vI) {
-                $this->lista[$i] = $this->lista[$i] + 1;
-                $i += 1;
-            }
-        } else {
-            return "Erro";
-        }
-    
+    // $qtd é a quantidade itens que você quer manter no array
+    public function removerItens($qtd) {
+        array_splice($this->lista, $qtd);
     }
 
     public function procurarItens($p) {
